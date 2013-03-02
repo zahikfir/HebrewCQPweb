d8afcdc3700fde725f4420e3d6ac4c56dcc0f494
root <root@nlpuser-VirtualBox.(none)>
2013-03-02 10:22:50 +0200

    Added userselection file which enable the sys admin to change the current user to admin for maintenance tasks.

---
+++ adm/userselection.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+/* This file selects the current user / switch between admin to regular users */
+
+// Changing the current user to admin will grant all users admin permissions and abilities
+
+/* 
+ * If you need to Upload corpus/Edit settings insert the admin user name
+ * else insert the __unknown_user value
+ */
+
+
+$current_username = '__unknown_user';
+
+?>
